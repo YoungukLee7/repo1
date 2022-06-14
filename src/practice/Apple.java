9f0ecc98bb46961e7804480e037cfe57779c7b8a
Gi7D-09 <Gi7D-09@DESKTOP-E60JRQ6>
2022-06-14 14:50:32 +0900

    apple에 d를 추가

--- src/practice/Apple.java
+++ src/practice/Apple.java
@@ -2,4 +2,15 @@ package practice;
 
 public class Apple {
 
+	int a;
+	int b;
+	int c;
+	int d;
+	
+	public Apple(int a, int b, int c, int d) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
 }
